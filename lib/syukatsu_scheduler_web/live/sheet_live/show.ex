8019051c976e16aae7f2daf2fa714bfa79a03d9d
minusno0708 <naoyao1014@gmail.com>
2023-08-22 00:16:22 +0900

    fix:sheet/showでcontentが改行されなくなったため修正

--- lib/syukatsu_scheduler_web/live/sheet_live/show.ex
+++ lib/syukatsu_scheduler_web/live/sheet_live/show.ex
@@ -20,5 +20,5 @@ defmodule SyukatsuSchedulerWeb.SheetLive.Show do
   defp page_title(:edit), do: "Edit Sheet"
 
   defp text_indention(nil), do: nil
-  defp text_indention(text), do: String.replace(text,  "\n", "<br />")
+  defp text_indention(text), do: String.replace(text,  "\n", "<br/>")
 end
