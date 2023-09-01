8196d2e42c9e5ee201df04beb1d35d3e6b0517c1
minusno0708 <naoyao1014@gmail.com>
2023-09-01 23:52:21 +0900

    chore:dockerでphoenixを起動する

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,11 @@
 FROM elixir:1.15.4-slim
 
 WORKDIR /app
+
+COPY . .
+
+RUN apt-get update && apt-get install -y make gcc && \
+    mix local.hex --force && \ 
+    mix archive.install hex phx_new 1.7.7 --force && \
+    mix deps.get && \
+    mix deps.compile
--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :syukatsu_scheduler, SyukatsuScheduler.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "syukatsu_scheduler_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
@@ -19,7 +19,7 @@ config :syukatsu_scheduler, SyukatsuScheduler.Repo,
 config :syukatsu_scheduler, SyukatsuSchedulerWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
