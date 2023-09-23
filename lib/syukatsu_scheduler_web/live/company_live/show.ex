7619453e77d32beedd04dfc2f3003282c9d134cf
minusno0708 <naoyao1014@gmail.com>
2023-09-24 02:48:48 +0900

    feat:企業の詳細ページで不正なユーザーでのログインをはじく

--- lib/syukatsu_scheduler_web/live/company_live/show.ex
+++ lib/syukatsu_scheduler_web/live/company_live/show.ex
@@ -4,8 +4,14 @@ defmodule SyukatsuSchedulerWeb.CompanyLive.Show do
   alias SyukatsuScheduler.Accounts
 
   @impl true
+  def mount(_params, %{"user_token" => user_token}, socket) do
+    {:ok, user_id} = Accounts.get_userid_from_usertoken(user_token)
+
+    {:ok, socket |> assign(:current_user_id, user_id)}
+  end
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign(:current_user_id, nil)}
   end
 
   @impl true
